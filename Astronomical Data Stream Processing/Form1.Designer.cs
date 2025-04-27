d64eee0eeeebd785a127c1eae790e1f5b198abcb
StudentFrank <M026187@tafe.wa.edu.au>
2025-04-27 11:49:46 +0800

    Coding and Testing completed

--- Astronomical Data Stream Processing/Form1.Designer.cs
+++ Astronomical Data Stream Processing/Form1.Designer.cs
@@ -29,153 +29,181 @@
         private void InitializeComponent()
         {
             this.ListBoxData = new System.Windows.Forms.ListBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBoxLoadSort = new System.Windows.Forms.GroupBox();
             this.ButtonLoadData = new System.Windows.Forms.Button();
             this.ButtonSort = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.GroupBoxEditSearch = new System.Windows.Forms.GroupBox();
             this.TextBoxInput = new System.Windows.Forms.TextBox();
             this.ButtonSearch = new System.Windows.Forms.Button();
             this.ButtonEdit = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
+            this.LabelInput = new System.Windows.Forms.Label();
+            this.LabelMessageDisplay = new System.Windows.Forms.Label();
+            this.TextBoxMessage = new System.Windows.Forms.TextBox();
+            this.LabelAstronomicalProcessingData = new System.Windows.Forms.Label();
+            this.GroupBoxLoadSort.SuspendLayout();
+            this.GroupBoxEditSearch.SuspendLayout();
             this.SuspendLayout();
             // 
             // ListBoxData
             // 
             this.ListBoxData.FormattingEnabled = true;
-            this.ListBoxData.ItemHeight = 20;
-            this.ListBoxData.Location = new System.Drawing.Point(23, 44);
-            this.ListBoxData.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ListBoxData.Location = new System.Drawing.Point(32, 42);
             this.ListBoxData.Name = "ListBoxData";
-            this.ListBoxData.Size = new System.Drawing.Size(300, 504);
+            this.ListBoxData.Size = new System.Drawing.Size(201, 329);
             this.ListBoxData.TabIndex = 0;
+            this.ListBoxData.SelectedIndexChanged += new System.EventHandler(this.ListBoxData_SelectedIndexChanged);
             // 
-            // groupBox1
+            // GroupBoxLoadSort
             // 
-            this.groupBox1.Controls.Add(this.ButtonLoadData);
-            this.groupBox1.Controls.Add(this.ButtonSort);
-            this.groupBox1.Location = new System.Drawing.Point(400, 20);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(250, 150);
-            this.groupBox1.TabIndex = 9;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Load/Sort";
+            this.GroupBoxLoadSort.Controls.Add(this.ButtonLoadData);
+            this.GroupBoxLoadSort.Controls.Add(this.ButtonSort);
+            this.GroupBoxLoadSort.Location = new System.Drawing.Point(267, 13);
+            this.GroupBoxLoadSort.Margin = new System.Windows.Forms.Padding(2);
+            this.GroupBoxLoadSort.Name = "GroupBoxLoadSort";
+            this.GroupBoxLoadSort.Padding = new System.Windows.Forms.Padding(2);
+            this.GroupBoxLoadSort.Size = new System.Drawing.Size(167, 98);
+            this.GroupBoxLoadSort.TabIndex = 9;
+            this.GroupBoxLoadSort.TabStop = false;
+            this.GroupBoxLoadSort.Text = "Load/Sort";
             // 
             // ButtonLoadData
             // 
-            this.ButtonLoadData.Location = new System.Drawing.Point(64, 36);
+            this.ButtonLoadData.Location = new System.Drawing.Point(43, 23);
+            this.ButtonLoadData.Margin = new System.Windows.Forms.Padding(2);
             this.ButtonLoadData.Name = "ButtonLoadData";
-            this.ButtonLoadData.Size = new System.Drawing.Size(120, 40);
+            this.ButtonLoadData.Size = new System.Drawing.Size(80, 26);
             this.ButtonLoadData.TabIndex = 8;
             this.ButtonLoadData.Text = "Load Data";
             this.ButtonLoadData.UseVisualStyleBackColor = true;
+            this.ButtonLoadData.Click += new System.EventHandler(this.ButtonLoadData_Click);
             // 
             // ButtonSort
             // 
-            this.ButtonSort.Location = new System.Drawing.Point(64, 84);
-            this.ButtonSort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButtonSort.Location = new System.Drawing.Point(43, 55);
             this.ButtonSort.Name = "ButtonSort";
-            this.ButtonSort.Size = new System.Drawing.Size(120, 40);
+            this.ButtonSort.Size = new System.Drawing.Size(80, 26);
             this.ButtonSort.TabIndex = 3;
             this.ButtonSort.Text = "Sort";
             this.ButtonSort.UseVisualStyleBackColor = true;
+            this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
             // 
-            // groupBox2
+            // GroupBoxEditSearch
             // 
-            this.groupBox2.Controls.Add(this.TextBoxInput);
-            this.groupBox2.Controls.Add(this.ButtonSearch);
-            this.groupBox2.Controls.Add(this.ButtonEdit);
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(400, 176);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(250, 200);
-            this.groupBox2.TabIndex = 10;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Edit/Search";
+            this.GroupBoxEditSearch.Controls.Add(this.TextBoxInput);
+            this.GroupBoxEditSearch.Controls.Add(this.ButtonSearch);
+            this.GroupBoxEditSearch.Controls.Add(this.ButtonEdit);
+            this.GroupBoxEditSearch.Controls.Add(this.LabelInput);
+            this.GroupBoxEditSearch.Location = new System.Drawing.Point(267, 114);
+            this.GroupBoxEditSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.GroupBoxEditSearch.Name = "GroupBoxEditSearch";
+            this.GroupBoxEditSearch.Padding = new System.Windows.Forms.Padding(2);
+            this.GroupBoxEditSearch.Size = new System.Drawing.Size(167, 130);
+            this.GroupBoxEditSearch.TabIndex = 10;
+            this.GroupBoxEditSearch.TabStop = false;
+            this.GroupBoxEditSearch.Text = "Edit/Search";
             // 
             // TextBoxInput
             // 
-            this.TextBoxInput.Location = new System.Drawing.Point(64, 45);
-            this.TextBoxInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TextBoxInput.Location = new System.Drawing.Point(43, 29);
             this.TextBoxInput.Name = "TextBoxInput";
-            this.TextBoxInput.Size = new System.Drawing.Size(120, 26);
+            this.TextBoxInput.Size = new System.Drawing.Size(81, 20);
             this.TextBoxInput.TabIndex = 1;
+            this.TextBoxInput.TextChanged += new System.EventHandler(this.TextBoxInput_TextChanged);
             // 
             // ButtonSearch
             // 
-            this.ButtonSearch.Location = new System.Drawing.Point(64, 81);
-            this.ButtonSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButtonSearch.Location = new System.Drawing.Point(43, 53);
             this.ButtonSearch.Name = "ButtonSearch";
-            this.ButtonSearch.Size = new System.Drawing.Size(120, 40);
+            this.ButtonSearch.Size = new System.Drawing.Size(80, 26);
             this.ButtonSearch.TabIndex = 2;
             this.ButtonSearch.Text = "Search";
             this.ButtonSearch.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
             // 
             // ButtonEdit
             // 
-            this.ButtonEdit.Location = new System.Drawing.Point(64, 131);
-            this.ButtonEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButtonEdit.Location = new System.Drawing.Point(43, 85);
             this.ButtonEdit.Name = "ButtonEdit";
-            this.ButtonEdit.Size = new System.Drawing.Size(120, 40);
+            this.ButtonEdit.Size = new System.Drawing.Size(80, 26);
             this.ButtonEdit.TabIndex = 4;
             this.ButtonEdit.Text = "Edit";
             this.ButtonEdit.UseVisualStyleBackColor = true;
+            this.ButtonEdit.Click += new System.EventHandler(this.ButtonEdit_Click);
             // 
-            // label1
+            // LabelInput
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 48);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Input:";
+            this.LabelInput.AutoSize = true;
+            this.LabelInput.Location = new System.Drawing.Point(5, 31);
+            this.LabelInput.Name = "LabelInput";
+            this.LabelInput.Size = new System.Drawing.Size(34, 13);
+            this.LabelInput.TabIndex = 5;
+            this.LabelInput.Text = "Input:";
             // 
-            // groupBox3
+            // LabelMessageDisplay
             // 
-            this.groupBox3.Controls.Add(this.ListBoxData);
-            this.groupBox3.Location = new System.Drawing.Point(20, 20);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(350, 575);
-            this.groupBox3.TabIndex = 12;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Astronomical Data Display";
+            this.LabelMessageDisplay.AutoSize = true;
+            this.LabelMessageDisplay.Location = new System.Drawing.Point(305, 253);
+            this.LabelMessageDisplay.Name = "LabelMessageDisplay";
+            this.LabelMessageDisplay.Size = new System.Drawing.Size(90, 13);
+            this.LabelMessageDisplay.TabIndex = 15;
+            this.LabelMessageDisplay.Text = "Message Display:";
+            // 
+            // TextBoxMessage
+            // 
+            this.TextBoxMessage.Location = new System.Drawing.Point(267, 269);
+            this.TextBoxMessage.Multiline = true;
+            this.TextBoxMessage.Name = "TextBoxMessage";
+            this.TextBoxMessage.ReadOnly = true;
+            this.TextBoxMessage.Size = new System.Drawing.Size(167, 102);
+            this.TextBoxMessage.TabIndex = 13;
+            this.TextBoxMessage.TextChanged += new System.EventHandler(this.TextBoxMessage_TextChanged);
+            // 
+            // LabelAstronomicalProcessingData
+            // 
+            this.LabelAstronomicalProcessingData.AutoSize = true;
+            this.LabelAstronomicalProcessingData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelAstronomicalProcessingData.Location = new System.Drawing.Point(58, 13);
+            this.LabelAstronomicalProcessingData.Name = "LabelAstronomicalProcessingData";
+            this.LabelAstronomicalProcessingData.Size = new System.Drawing.Size(148, 13);
+            this.LabelAstronomicalProcessingData.TabIndex = 16;
+            this.LabelAstronomicalProcessingData.Text = "Astronomical Processing Data";
             // 
             // AstronomicalProcessingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 617);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ClientSize = new System.Drawing.Size(452, 401);
+            this.Controls.Add(this.LabelAstronomicalProcessingData);
+            this.Controls.Add(this.ListBoxData);
+            this.Controls.Add(this.TextBoxMessage);
+            this.Controls.Add(this.LabelMessageDisplay);
+            this.Controls.Add(this.GroupBoxEditSearch);
+            this.Controls.Add(this.GroupBoxLoadSort);
             this.Name = "AstronomicalProcessingForm";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Astronomical Processing";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
+            this.GroupBoxLoadSort.ResumeLayout(false);
+            this.GroupBoxEditSearch.ResumeLayout(false);
+            this.GroupBoxEditSearch.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox ListBoxData;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox GroupBoxLoadSort;
         private System.Windows.Forms.Button ButtonLoadData;
         private System.Windows.Forms.Button ButtonSort;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox GroupBoxEditSearch;
         private System.Windows.Forms.TextBox TextBoxInput;
         private System.Windows.Forms.Button ButtonSearch;
         private System.Windows.Forms.Button ButtonEdit;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label LabelInput;
+        private System.Windows.Forms.Label LabelMessageDisplay;
+        private System.Windows.Forms.TextBox TextBoxMessage;
+        private System.Windows.Forms.Label LabelAstronomicalProcessingData;
     }
 }
 
--- Astronomical Data Stream Processing/Form1.cs
+++ Astronomical Data Stream Processing/Form1.cs
@@ -4,9 +4,17 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+// Frank Padovan, Astronomical Processing, Sprint 1
+// Date: 21/04/25
+// Astronomical Data Stream Processing
+// A forms based GUI application which displays recorded neutrino interactions per hour as an integer value for a 24 hour period.
+// Data is displayed in a list box and uses text boxes for user input and for displaying messages. Buttons are used for data processing.
+// Data processing includes the ability to load, view, sort, search and edit the neutrino interaction data set.
 
 namespace Astronomical_Data_Stream_Processing
 {
@@ -17,9 +25,253 @@ namespace Astronomical_Data_Stream_Processing
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        // Global variables
+        static int arraySize = 24;
+        private int[] neutrinoInteractions = new int[arraySize];
+        int listBoxSelected;
+        bool allZero = true;
+        bool nothingSelected = true;
+        Random rand = new Random();
+
+        // Method to deal with edit button being clicked
+        private void ButtonEdit_Click(object sender, EventArgs e)
+        {
+            // Clear the message text box
+            TextBoxMessage.Clear();
+            // Check to see if the neutrino data has been loaded
+            CheckIfEmpty();
+            // If the data is empty prompt user to load data first
+            if (allZero == true)
+            {
+                TextBoxMessage.Text = "ERROR:" + "\r\n" + "No data loaded" + "\r\n\r\n" + "Load data to edit";
+                return;
+            }
+            // Check if a value has been selected in the list box
+            ListBoxSelection();
+            if (nothingSelected == false)
+            {
+                // Check to see if data entered into input text box is valid
+                try
+                {
+                    neutrinoInteractions[listBoxSelected] = Convert.ToInt32(TextBoxInput.Text);
+                    // Inform the user which index has been edited
+                    TextBoxMessage.Text = "Value edited at index: " + listBoxSelected + "\r\n";
+                }
+                // If it's not valid, display error to user
+                catch
+                {
+                    TextBoxMessage.Text = "ERROR:" + "\r\n" + "No valid input data" + "\r\n\r\n" + "Enter integer value into input";
+                }
+                // Display the edited value in the list box
+                DisplayData();   
+            }
+            // If a value hasn't been selected in the list box, display error to user
+            else if (nothingSelected == true)
+            {
+                TextBoxMessage.Text = "ERROR:" + "\r\n" + "Value to edit not selected" + "\r\n\r\n" + "Select value to edit";
+            }
+            // Clear user selection in list box
+            ListBoxData.ClearSelected();
+            nothingSelected = true;
+        }
+        // Method to handle load data button click
+        private void ButtonLoadData_Click(object sender, EventArgs e)
+        {
+            // Clear message display
+            TextBoxMessage.Clear();
+            // Load data into neutrinoInteractions array
+            FillArray();
+        }
+        // Method to handle search button click
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            // Clear message display
+            TextBoxMessage.Clear();
+            // Check to see if data has been loaded
+            CheckIfEmpty();
+            // If data has not been loaded, display error to user
+            if (allZero == true)
+            {
+                TextBoxMessage.Text = "ERROR:" + "\r\n" + "No data loaded" + "\r\n\r\n" + "Load data to search";
+                return;
+            }
+            // Perform binary search
+            BinarySearch();
+        }
+        // Method to handle sort button click
+        private void ButtonSort_Click(object sender, EventArgs e)
+        {
+            // Clear message display
+            TextBoxMessage.Clear();
+            // Check to see if data has been loaded
+            CheckIfEmpty();
+            // If data has not been loaded, display error to user
+            if (allZero == true)
+            {
+                TextBoxMessage.Text = "ERROR:" + "\r\n" + "No data loaded" + "\r\n\r\n" + "Load data to sort";
+                return;
+            }
+            // Perform bubble sort 
+            BubbleSort();
+            // Display sorted values in the list box
+            DisplayData();
+        }
+
+        private void ListBoxData_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void TextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxMessage_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+        // Method to search the data stored in neutrinoInteractions using binary search
+        private void BinarySearch()
+        {
+            // Initialise variables
+            int min = 0;
+            int max = arraySize - 1;
+            int target;
+            // Check to makes sure user has input an integer value into the input text box
+            try
+            {
+                target = Convert.ToInt32(TextBoxInput.Text);
+            }
+            // If the user has not input an integer value into the input text box, display error in message box
+            catch
+            {
+                TextBoxMessage.Text = "ERROR:" + "\r\n" + "No input data to search for" + "\r\n\r\n" + "Enter an integer value to search";
+                return;
+            }
+                     
+            while (min <= max)
+            {
+                // Calculate the middle value of the neutrinoInteractions array
+                int mid = (min + max) / 2;
+                // Check to see if the search target matches the middle value of the neutrinoInteractions array
+                if (target == neutrinoInteractions[mid])
+                {
+                    // If it matches then display to user that the search value has been found
+                    TextBoxMessage.Text = target + " Found at index " + mid + "\r\n";
+                    // Clear the input text box
+                    TextBoxInput.Clear();
+                    return;
+                }
+                // If the search target is less than the middle value then adjust the max value to be the previously calculated middle value - 1
+                else if (target < neutrinoInteractions[mid])
+                {
+                    max = mid - 1;
+                }
+                // If the search target is greater than the middle value then adjust the min value to be the previously calculated middle value + 1
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            // If the search target is not found, display error to user
+            TextBoxMessage.Text = "ERROR:" + "\r\n" + "Search value does not exist.";
+            // Clear the input text box
+            TextBoxInput.Clear();
+            // Give focus to the input text box
+            TextBoxInput.Focus();
+        }
+        // Method to sort the data stored in neutrinoInteractions using bubble sort
+        private void BubbleSort()
+        {
+            // Initialise variables
+            bool swapped;
+            int temp;
+
+            // do until swapped is true
+            do
+            {
+                swapped = false;
+                // For all elements of the neutrinoInteractions array
+                for (int k = 0; k < arraySize - 1; k++)
+                {
+                    // Check to see if element k is less than element k+1
+                    if (neutrinoInteractions[k] > neutrinoInteractions[k + 1])
+                    {
+                        // Swap element k with element k+1
+                        temp = neutrinoInteractions[k];
+                        neutrinoInteractions[k] = neutrinoInteractions[k + 1];
+                        neutrinoInteractions[k + 1] = temp;
+                        // Flag swapped
+                        swapped = true;
+                    }
+                }
+            }
+            while (swapped == true);
+        }
+        // Method to check if the neutrinoInteractions array has been populated with neutrino interaction values
+        private void CheckIfEmpty()
+        {
+            // Check each element in array
+            foreach (int value in neutrinoInteractions)
+            {   
+                // Is the element non-zero
+                if (value != 0)
+                {
+                    // Set allZero to false
+                    allZero = false;
+                    break;
+                }
+            }
+        }
+        // Method to display the contents of neutrinoInteractions in the list box
+        private void DisplayData()
+        {
+            // Clear list box
+            ListBoxData.Items.Clear();
+            // Display each element of neutrinoInteractions
+            for (int x = 0; x < arraySize; x++)
+            {
+                ListBoxData.Items.Add(" " + neutrinoInteractions[x]);
+            }
+        }
+        // Method to fill the neutrinoInteractions array with random values from 10 to 90
+        private void FillArray()
+        {
+            for (int x = 0; x < arraySize; x++)
+            {
+                for (int y = 0; y < arraySize;  y++)
+                {
+                    int randomInteger = rand.Next(10, 90);
+                    // Check to see if new random number is a duplicate. If it is then regenerate, otherwise store in neutrinoInteractions
+                    if (neutrinoInteractions.Contains(randomInteger))
+                    {
+                        y--;
+                    }
+                    else
+                    {
+                        neutrinoInteractions[y] = randomInteger;
+                    }
+                }
+            }
+            // Display contents of neutrinoInteractions in the list box
+            DisplayData();
+        }
+        // Method to check if a list box element has been selected
+        private void ListBoxSelection()
+        {
+            // Check the entire array to see if any items have been selected
+            for (int x = 0; x < arraySize; x++)
+            {   
+                // Check which array elemnent has been selected
+                if (ListBoxData.GetSelected(x))
+                {
+                    // Record the index value that has been selected
+                    listBoxSelected = x;
+                    // As something has been selected, nothingSelected becomes false
+                    nothingSelected = false;
+                }
+            }
         }
     }
 }
